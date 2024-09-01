b596a0d594de2f8913fda7ca8f07018b097e2b22
V0000DY <proektant.burenie@gmail.com>
2024-09-01 12:27:48 +0700

    update index.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
